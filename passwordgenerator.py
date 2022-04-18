8f262bd4c550950477c399799425a545ed0d5fe5
ShafferMichael <shaffer.michael@ufl.edu>
2022-04-18 10:38:42 -0400

    update

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 class passwordGenerator:
     def __init__(self, passwords):
         self.passwords = passwords
@@ -5,12 +8,18 @@ class passwordGenerator:
     def __repr__(self):
         return self.passwords
 
+    def insert_str(string, str_to_insert, index):
+        return string[:index] + str_to_insert + string[index:]
+
     def generate(self, keyword, length):
         spaces_to_fill = length - len(keyword)
+        symbols = list("!@#$%^&*()1234567890_-+=")
         if len(keyword) >= length:
             return -1
         else:
-            for char in keyword:
+            for x in range(100000):
+                random_char = random.choice(symbols)
+
 
 
 
