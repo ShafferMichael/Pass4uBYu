7e12c6c9f9c9b5217a162ba2b7635c09f08def5d
ShafferMichael <shaffer.michael@ufl.edu>
2022-04-18 10:21:36 -0400

    update

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import UI
 import priority_queue
 import heapsort
 
+# take in value
+keyword = "Buddy"
+length = 11
+object1 = passwordgenerator
+passwords = object1.passwordGenerator.generate(keyword, length)
--- passwordgenerator.py
+++ passwordgenerator.py
@@ -3,6 +3,16 @@ class passwordGenerator:
         self.passwords = passwords
 
     def __repr__(self):
-        return self.password
+        return self.passwords
+
+    def generate(self, keyword, length):
+        spaces_to_fill = length - len(keyword)
+        if len(keyword) >= length:
+            return -1
+        else:
+            for char in keyword:
+
+
+
 
 
