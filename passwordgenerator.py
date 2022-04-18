7f481e2e40045e5b5c19371ae95546e0c532a9a2
ShafferMichael <shaffer.michael@ufl.edu>
2022-04-18 11:52:51 -0400

    v1.1 of passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -2,14 +2,15 @@ import random
 
 symbols = list("!@#$%^&*()1234567890_")
 
-class passwordGenerator:
+
+class passwordgenerator:
     def __init__(self, passwords):
         self.passwords = passwords
 
     def __repr__(self):
         return self.passwords
 
-    def insert_str(self,keyword, str_to_insert, index):
+    def insert_str(self, keyword, str_to_insert, index):
         return keyword[:index] + str_to_insert + keyword[index:]
 
     def rank_password(self, password):
@@ -23,28 +24,22 @@ class passwordGenerator:
                 value -= 1
         return value
 
-                ## repeating duplicates
+        # repeating duplicates
 
 
+def generate(self, keyword, length):
+    password = ''
+    spaces_to_fill = length - len(keyword)
 
-    def generate(self, keyword, length):
-        global password
-        spaces_to_fill = length - len(keyword)
+    if len(keyword) >= length:
+        return -1
+    else:
+        for x in range(100000):
+            for y in range(spaces_to_fill):
+                random_int = random.randint(0, length - 1)
+                random_char = random.choice(symbols)
+                password = self.insert_str(keyword, random_char, random_int)
 
-        if len(keyword) >= length:
-            return -1
-        else:
-            for x in range(100000):
-                for y in range(spaces_to_fill):
-                    random_int = random.randint(0, length-1)
-                    random_char = random.choice(symbols)
-                    password = self.insert_str(keyword, random_char, random_int)
-
-                rank = self.rank_password(password)
-                self.passwords[password] = rank
-            return self.passwords
-
-
-
-
-
+            rank = self.rank_password(password)
+            self.passwords[password] = rank
+        return self.passwords
