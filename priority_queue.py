efb461015d8a81722b9b45e7d1d951e6a316d4a6
waderichman <waderichman444@gmail.com>
2022-04-18 15:37:49 -0400

    ideas for pq

--- priority_queue.py
+++ priority_queue.py
@@ -1,3 +1,20 @@
-from queue import PriorityQueue
 
-q = PriorityQueue()
+from queue import PriorityQueue
+from passwordgenerator import passwordGenerator
+
+
+for i in range(0,9):
+
+#load first 10 into PQ (11 starting size)
+
+#create heap (11 starting size)
+
+    for i in range(10,100000):
+#add strength to PQ
+#temp = PQ.pop;
+# if temp == strengths[i] continue
+#remove temp from heap
+#add new wrapper of strengths[i] & passwords[i]
+#re-sort heap
+
+# just some thoughts
