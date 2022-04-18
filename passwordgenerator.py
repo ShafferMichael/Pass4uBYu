6f451050400e75523393096a8da2bae66e9f2b58
ShafferMichael <shaffer.michael@ufl.edu>
2022-04-18 11:34:23 -0400

    v1 of passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,5 +1,6 @@
 import random
-import string
+
+symbols = list("!@#$%^&*()1234567890_")
 
 class passwordGenerator:
     def __init__(self, passwords):
@@ -8,12 +9,28 @@ class passwordGenerator:
     def __repr__(self):
         return self.passwords
 
-    def insert_str(string, str_to_insert, index):
-        return string[:index] + str_to_insert + string[index:]
+    def insert_str(self,keyword, str_to_insert, index):
+        return keyword[:index] + str_to_insert + keyword[index:]
+
+    def rank_password(self, password):
+        value = 0
+        for char in password:
+            if char in symbols:
+                value += 2
+            else:
+                value += 1
+            if password.count(char) > 1:
+                value -= 1
+        return value
+
+                ## repeating duplicates
+
+
 
     def generate(self, keyword, length):
+        global password
         spaces_to_fill = length - len(keyword)
-        symbols = list("!@#$%^&*()1234567890_")
+
         if len(keyword) >= length:
             return -1
         else:
@@ -23,5 +40,11 @@ class passwordGenerator:
                     random_char = random.choice(symbols)
                     password = self.insert_str(keyword, random_char, random_int)
 
+                rank = self.rank_password(password)
+                self.passwords[password] = rank
+            return self.passwords
 
-    def rank_password(self,):
+
+
+
+
