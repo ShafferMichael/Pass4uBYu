4983fbdc2a03346da4bc249c87873a5599b861c2
ShafferMichael <shaffer.michael@ufl.edu>
2022-04-19 12:58:24 -0400

    working on heapsort.py

--- heapsort.py
+++ heapsort.py
@@ -1,2 +1,8 @@
 # https://www.geeksforgeeks.org/how-to-maintain-dictionary-in-a-heap-in-python/
 import heapq as hq
+import heapq
+
+def with_heapq(the_dict):
+    items = [(-value, key) for key, value in the_dict.items()]
+    smallest = heapq.nsmallest(10, items)
+    return [-value for value, key in smallest]
